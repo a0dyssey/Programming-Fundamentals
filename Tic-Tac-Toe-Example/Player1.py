dd56b6742f419922f525ede975866c625aca91a6
a0dyssey <gawdprod@gmail.com>
2024-11-06 13:54:49 -0500

    sent update tp p1

--- Tic-Tac-Toe-Example/Player1.py
+++ Tic-Tac-Toe-Example/Player1.py
@@ -2,6 +2,10 @@
 #
 # author: Stephen Adams
 # date  : 2024-10-19
+
+
+#changed by me
+
 #
 # This file contains the code for Player 1 in a Tic-Tac-Toe game. It is intentionally not simplified or generalized to
 # attempt to avoid merge conflicts between the partners.
